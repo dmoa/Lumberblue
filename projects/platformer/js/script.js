3d7810e1424d3c68937459ab3e3d9f0cb205ab68
Stan <spammyboy@protonmail.com>
2018-11-17 11:34:53 +0000

    rewfd
    efd

--- projects/platformer/js/script.js
+++ projects/platformer/js/script.js
@@ -1,9 +1,9 @@
-let playery = 50;
+let playery = -50;
 let playerLength = 50;
 let playerx = 50;
 let playervy = 7;
 let playervx = 5;
-let acceleration = 0.01;
+let acceleration = 0.3;
 let jumping = true;
 let boundaryThickness = 5;
 
@@ -22,15 +22,15 @@ function setup() {
   playerx = width / 2 - playerLength / 2;
 
   let platform1 = new Platform(boundaryThickness, height - boundaryThickness - 30, 120, 80);
-  let platform2 = new Platform(200, 350, 70, 50 - boundaryThickness);
-  let platform3 = new Platform(400, 350, 70, 50 - boundaryThickness);
+  let platform2 = new Platform(150, 350, 70, 50 - boundaryThickness);
+  let platform3 = new Platform(400, 250, 70, 50 - boundaryThickness);
+  let platform4 = new Platform(300, 280, 70, 50 - boundaryThickness);
   platforms.push(platform1);
   platforms.push(platform2);
   platforms.push(platform3);
-
+  platforms.push(platform4);
   for (let i = 0; i < numparticles; i++) {
-    let p = new Particle();
-    particles.push(p);
+    particles.push(new Particle());
   }
 }
 
@@ -38,7 +38,7 @@ function setup() {
 function draw() {
   background("#000000");
 
-
+  //rain drawing and updating
   for (let i = particles.length - 1; i >= 0; i--) {
     particles[i].update();
     particles[i].show();
@@ -49,13 +49,15 @@ function draw() {
     }
   }
 
+
+  //drawing boundaries of canvas
   fill("#888888");
   rect(0, 0, boundaryThickness, height);
   rect(0, height - boundaryThickness, width, height);
   rect(width - boundaryThickness, 0, boundaryThickness, height);
 
   //player
-  fill("#FFFFFF");
+  fill("#666666");
   rect(playerx, playery, playerLength, playerLength);
 
   //update player
@@ -63,29 +65,30 @@ function draw() {
   if (playery + playervy >= height - playerLength - boundaryThickness) {
     playery = height - playerLength - boundaryThickness;
   }
-
+  if (playerx - playervx < boundaryThickness) {
+    playerx = boundaryThickness;
+  }
+  if (playerx + playerLength + playervx > width - boundaryThickness) {
+    playerx = width - boundaryThickness - playerLength;
+  }
   platformstfL = 0;
   platformstfR = 0;
   platformstfF = 0;
+
   for (let i = 0; i < platforms.length; i++) {
-    if (playerx > boundaryThickness &&
-      (playerx > platforms[i].x + platforms[i].width ||
-        playerx + playerLength <= platforms[i].x ||
-        playery + playerLength <= platforms[i].y)) {
-      platformstfL++;
-    }
-
-
-    if (playerx + playerLength < width - boundaryThickness &&
-      (playerx + playerLength < platforms[i].x ||
-        playerx >= platforms[i].x + platforms[i].width ||
-        playery + playerLength <= platforms[i].y)) {
-      platformstfR++;
+    if ([playervy] < 0 && playery > platforms[i].y &&
+      playery + playervy < platforms[i].y + platforms[i].height &&
+      ((playerx < platforms[i].x + platforms[i].width &&
+        playerx > platforms[i].x) || (playerx + playerLength > platforms[i].x &&
+        playerx + playerLength < platforms[i].x + platforms[i].width))) {
+      playery = platforms[i].y + platforms[i].height;
+      playervy = 0;
     }
 
     //corrects the little overlaps due to the decimal acceleration +  moving left and right
+
     if (playery + playervy + playerLength > platforms[i].y &&
-      playery + playerLength < platforms[i].y + platforms[i].height / 2 &&
+      playery + playerLength < platforms[i].y + platforms[i].height &&
       playerx + playerLength > platforms[i].x &&
       playerx < platforms[i].x + platforms[i].width) {
       playery = platforms[i].y - playerLength;
@@ -93,24 +96,44 @@ function draw() {
 
     //if all hope is lost in finding a bug it's prob here
     if (playerx + playervx + playerLength > platforms[i].x &&
-      playery + playerLength > platforms[i].y &&
-      playerx + playerLength < platforms[i].x) {
+      (playery + playerLength > platforms[i].y &&
+        playery < platforms[i].y + platforms[i].height) &&
+      playerx + playerLength < platforms[i].x + platforms[i].width) {
       playerx = platforms[i].x - playerLength;
     }
 
     if (playerx - playervx < platforms[i].x + platforms[i].width &&
       playery + playerLength > platforms[i].y &&
+      playery < platforms[i].y + platforms[i].height &&
       playerx > platforms[i].x) {
       playerx = platforms[i].x + platforms[i].width;
     }
+    // rwgfdasccdsfhdgfbsvdc\
+    if (playerx > boundaryThickness &&
+      (playerx > platforms[i].x + platforms[i].width ||
+        playerx + playerLength <= platforms[i].x ||
+        playery + playerLength <= platforms[i].y ||
+        playery > platforms[i].y + platforms[i].height)) {
+      platformstfL++;
+    }
 
 
+    if (playerx + playerLength < width - boundaryThickness &&
+      (playerx + playerLength < platforms[i].x ||
+        playerx >= platforms[i].x + platforms[i].width ||
+        playery + playerLength <= platforms[i].y ||
+        playery > platforms[i].y + platforms[i].height)) {
+      platformstfR++;
+    }
 
     // can i fall?
     if (playery < height - playerLength - boundaryThickness &&
       ((playerx + playerLength <= platforms[i].x ||
           playerx >= platforms[i].x + platforms[i].width) ||
         playery + playerLength < platforms[i].y)) {
+      platformstfF++;
+    } else if (playery < height - playerLength - boundaryThickness &&
+      playery + playerLength >= platforms[i].y + platforms[i].height) {
       platformstfF++;
     }
   }
@@ -124,8 +147,8 @@ function draw() {
 
   if (platformstfF == platforms.length) {
     if (!jumping) {
-      acceleration = 1;
       jumping = true;
+      acceleration = 0.3;
     }
     playery += playervy;
     playervy += acceleration;
@@ -136,18 +159,16 @@ function draw() {
   }
 
 
-
-
-//platforms drawing
-fill("#FFFFFF");
-for (var i = 0; i < platforms.length; i++) {
-  platforms[i].show();
-}
+  //platforms drawing
+  fill("#FFFFFF");
+  for (var i = 0; i < platforms.length; i++) {
+    platforms[i].show();
+  }
 }
 
 //jumping
 function keyPressed() {
-  if (keyCode == 32 && !jumping) {
+  if ((keyCode == 32) && !jumping) {
     playervy = -7;
     playery -= 10;
     acceleration = 0.3;
